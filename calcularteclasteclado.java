0d6ecd28c07a2c08f99eae271fab4c107e49f019
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2024-06-27 17:02:26 -0300

    Create calcularteclasteclado.java

---
+++ calcularteclasteclado.java
@@ -0,0 +1,9 @@
+public class ContagemTeclasTeclado {
+
+    public static void main(String[] args) {
+        // Número aproximado de teclas em um teclado padrão
+        int numeroTeclas = 104;
+
+        System.out.println("Um teclado padrão tem aproximadamente " + numeroTeclas + " teclas.");
+    }
+}
